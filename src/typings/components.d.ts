771fb8b3a1ed5f775aedc40cdbb4b126b173ac7b
zhao.gaosheng <zhao.gaosheng@nint.com>
2022-05-02 02:30:09 +0800

    feat: add button
    go to top button
    
    close #2

--- src/typings/components.d.ts
+++ src/typings/components.d.ts
@@ -7,6 +7,8 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Bookmarks: typeof import('./../components/Bookmarks.vue')['default']
     Footer: typeof import('./../components/Footer.vue')['default']
+    GoTo: typeof import('./../components/GoTo.vue')['default']
+    GoTop: typeof import('./../components/GoTop.vue')['default']
     Header: typeof import('./../components/Header.vue')['default']
     Plum: typeof import('./../components/Plum.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
