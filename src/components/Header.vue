e59b726c6d84be5d5299c4344e2433a1b4ccfde4
zhao.gaosheng <zhao.gaosheng@nint.com>
2022-05-02 19:37:49 +0800

    feat: add plugin
    add vue-i18n

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,23 +13,23 @@ const localeIcon = computed(() => {
 </script>
 
 <template>
-  <header class="text-xl h-bg border-b border-gray-200 dark:border-gray-200/20">
-    <nav class="flex justify-between items-center container mx-auto h-56px">
-      <router-link class="flex items-center mx-2" to="/" title="首页">
-        <SvgIcon class="w-40px h-40px" name="logo" />
+  <header class="border-b h-bg border-gray-200 text-xl dark:border-gray-200/20">
+    <nav class="container flex mx-auto h-56px justify-between items-center">
+      <router-link class="flex mx-2 items-center" to="/" title="首页">
+        <SvgIcon class="h-40px w-40px" name="logo" />
         Bookmarks
       </router-link>
 
       <div class="flex items-center">
-        <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+        <button class="mx-2 icon-btn !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
           <div i="carbon-sun dark:carbon-moon" />
         </button>
-        <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_langs')" @click="toggleLocales">
+        <button class="mx-2 icon-btn !outline-none" :title="t('button.toggle_langs')" @click="toggleLocales">
           <div v-show="localeIcon" i="clarity-language-solid" />
           <div v-show="!localeIcon" i="clarity-language-line" />
         </button>
 
-        <a class="icon-btn mx-2" rel="noreferrer" title="GitHub" href="https://github.com/imitation-dev/bookmarks" target="_blank">
+        <a class="mx-2 icon-btn" rel="noreferrer" title="GitHub" href="https://github.com/imitation-dev/bookmarks" target="_blank">
           <div i-carbon-logo-github />
         </a>
       </div>
